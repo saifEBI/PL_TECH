3a04cc4a65a939c75d487b8a2370a3d73ec0ee0c
Saif Ur-Rehman <saifu@ebi.ac.uk>
2018-01-17 15:43:01 +0000

    initial commit 1

--- script.R
+++ script.R
@@ -1,5 +1,6 @@
 source("https://bioconductor.org/biocLite.R")
 biocLite("ExpressionAtlas")
 experimentAccessions=c('E-MTAB-5214', 'E-MTAB-513', 'E-MTAB-2836', 'E-MTAB-3716', 'E-MTAB-4344')
-getAtlasData(experimentAccessions)
+allExps=getAtlasData(experimentAccessions)
+
 
