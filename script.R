842a5555ddf98b4e645a5c6d09a55139467074e1
Saif Ur-Rehman <saifu@ebi.ac.uk>
2018-01-18 16:57:55 +0000

    Working

--- script.R
+++ script.R
@@ -3,4 +3,34 @@ biocLite("ExpressionAtlas")
 experimentAccessions=c('E-MTAB-5214', 'E-MTAB-513', 'E-MTAB-2836', 'E-MTAB-3716', 'E-MTAB-4344')
 allExps=getAtlasData(experimentAccessions)
 
+testGenes=c()
+testParts=c()
+
+for (acc in experimentAccessions)
+{
+	testGenes=c(testGenes,(rownames(assays(allExps[[acc]]$rnaseq)$counts)))
+}
+
+uniGenes=unique(testGenes)
+rm(testGenes)
+
+testParts=c()
+for (acc in experimentAccessions)
+{
+	testParts=c(testParts,(allExps[[acc]]$rnaseq$organism_part))
+}
+
+uniqueParts=unique(testParts)
+rm(testParts)
+
+
+
+for (acc in experimentAccessions)
+{
+	w=acc;
+	exp = allExps[[w ]];
+	sumexp <- exp$rnaseq;
+	x=head( assays( sumexp )$counts );
+	print(colnames(x));
+	index=index+1}
 
